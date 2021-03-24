396b55cc2de349694642ccf87860c6e2b4080ff1
Starman <30315137+Starman3787@users.noreply.github.com>
2021-03-24 19:28:23 +0000

    nucleo-144 board pins

---
+++ headers/main.h
@@ -0,0 +1,11 @@
+#ifndef _VVC_MAIN_H
+#define _VVC_MAIN_H
+
+#include <stdint.h>
+#include "./stm32f767xx.h"
+
+// Define GPIO pin mappings for the LED and button
+#define BUTTON_PIN  (13)    // GPIOC
+#define LED_PIN     (7)     // GPIOB
+
+#endif
